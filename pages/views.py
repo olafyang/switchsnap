fd21d2eb8fe30323f3e51d263e826a9979b7e7bd
olafyang <44876184+olafyang@users.noreply.github.com>
2020-10-21 21:35:06 +0200

    ui update

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.models import User
-from django.contrib import messages
 from img_storage.models import img_link_storage
 from django.db.models import Max
 from api_key import *
@@ -17,29 +16,19 @@ api = tweepy.API(auth)
 def homepage_view(request):
     if request.user.is_authenticated:
         context = {
-            'handle': request.user.social_auth.get(provider='twitter').extra_data['access_token']['screen_name']
+            'handle': request.user.social_auth.get(provider='twitter').extra_data['access_token']['screen_name'],
         }
         return render(request, "home.html", context)
     else:
         return render(request, "home.html", {})
 
 
-def demo_view(request, number_of_result=50):
+def demo_view(request, number_of_result=40):
     db_user_objects = img_link_storage.objects.filter(media_owner=User.objects.get(username='olafyxyz')).order_by(
         'twitter_media_id')
-    db_max_id = db_user_objects.aggregate(Max('tweet_id'))['tweet_id__max']  # find last entry in db
     db_count = db_user_objects.count()
     nss_images = db_user_objects.reverse()[:number_of_result]
 
-    # get tweets from twitter api
-    tweets = api.user_timeline(id='1231405854194356224', tweet_mode="extended", since_id=db_max_id)
-    for tweet in tweets:
-        if tweet.source == "Nintendo Switch Share":
-            for extended_entity_media in tweet.extended_entities['media']:
-                if extended_entity_media['type'] == 'photo':
-                    nss_images[tweet.id] = extended_entity_media['media_url_https']
-                else:
-                    continue
     context = {
         'img_dict': nss_images,
         'n': number_of_result,
@@ -48,7 +37,7 @@ def demo_view(request, number_of_result=50):
     return render(request, "demo.html", context)
 
 
-def gallery_view(request, number_of_result=50):
+def gallery_view(request, number_of_result=40):
     # check if user is logged in
     if not request.user.is_authenticated:
         return redirect('/')
@@ -68,7 +57,6 @@ def gallery_view(request, number_of_result=50):
         if tweet.source == "Nintendo Switch Share":
             for extended_entity_media in tweet.extended_entities['media']:
                 if extended_entity_media['type'] == 'photo':
-                    nss_images[tweet.id] = extended_entity_media['media_url_https']
                     db_entry = img_link_storage(media_owner=request.user,
                                                 media_url=extended_entity_media['media_url_https'], tweet_id=tweet.id,
                                                 twitter_media_id=extended_entity_media['id'])
@@ -80,6 +68,7 @@ def gallery_view(request, number_of_result=50):
         'img_dict': nss_images,
         'n': number_of_result,
         'db_count': db_count,
+        'handle': request.user.social_auth.get(provider='twitter').extra_data['access_token']['screen_name'],
     }
 
     return render(request, "gallery.html", context)
@@ -90,12 +79,16 @@ def img_view(request, media_id):
     context = {
         'img_link': db_img_obj.media_url,
         'tweet_id': db_img_obj.tweet_id,
+        'handle': request.user.social_auth.get(provider='twitter').extra_data['access_token']['screen_name'],
     }
     return render(request, 'imgview.html', context)
 
 
 def about_view(request):
-    return render(request, "about.html", {})
+    context = {
+        'handle': request.user.social_auth.get(provider='twitter').extra_data['access_token']['screen_name']
+    }
+    return render(request, "about.html", context)
 
 
 def logout(request):
@@ -104,4 +97,7 @@ def logout(request):
 
 
 def privacy(request):
-    return render(request, "privacy.html")
+    context = {
+        'handle': request.user.social_auth.get(provider='twitter').extra_data['access_token']['screen_name']
+    }
+    return render(request, "privacy.html", context)
