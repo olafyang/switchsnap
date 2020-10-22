e8de82dcc337da01e1b8761e5b796ee0fa368aeb
olafyang <44876184+olafyang@users.noreply.github.com>
2020-10-22 02:00:45 +0200

    homepage update

--- python/test.py
+++ python/test.py
@@ -11,7 +11,7 @@ api = tweepy.API(auth)
 #lists of twitter meida url with "Nintendo Switch Share" as source
 nssImgs_status_id = []
 nssImgs_url = []
-public_tweets = api.user_timeline(tweet_mode="extended")
+public_tweets = api.user_timeline(tweet_mode="extended", count=100)
 
 for tweet in public_tweets:
     if tweet.source == "Nintendo Switch Share":
@@ -22,10 +22,4 @@ for tweet in public_tweets:
             else:
                 continue
 
-print(nssImgs_status_id)
-print(nssImgs_url)
-
-
-
-
-
+print(len(nssImgs_url))
