c487f30c820e53c7c92aeb8ce3f1662852372a7e
olafyang <44876184+olafyang@users.noreply.github.com>
2020-11-13 19:38:42 +0100

    update

--- css_production/tailwind.config.js
+++ css_production/tailwind.config.js
@@ -19,7 +19,6 @@ module.exports = {
   theme: {
     extend: {
       spacing: {
-        '9/16': '56.25%',
       }
     },
   },
--- switchsnap/settings.py
+++ switchsnap/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = '***REMOVED***'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '***REMOVED***',
+]
 
 # Application definition
 
