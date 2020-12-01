b521a3f6a03db15f182c310804937f9c53fdb4c7
olafyang <ian9703@gmail.com>
2020-12-01 21:36:34 +0100

    merging dev

--- switchsnap/settings.py
+++ switchsnap/settings.py
@@ -24,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '***REMOVED***'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
-    '127.0.0.1',
     '***REMOVED***',
 ]
 
